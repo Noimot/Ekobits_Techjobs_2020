94e3e0915b32fe9135cef4fba5c4d76675c3b9ab
Noimot <kikkyal@gmail.com>
2021-02-11 22:07:37 +0100

    changes committed

--- node-express/nodejs-exercise/index.js
+++ node-express/nodejs-exercise/index.js
@@ -2,37 +2,43 @@ const fs = require("fs");
 const request = require("request");
 const prompt = require("prompt");
 
-let searchTerm;
-let searchResponse;
+
 
 prompt.start()
 
 prompt.get("search", function (err, result) {
     if (err) {
         console.log(err)
-    }
+    }console.log(result)
     searchTerm = result.search;
     console.log(`search: ${searchTerm}`)
 
+     
+    
+    request({
+        headers:{
+            'Accept':'application/json'
+        },
+        uri:`https://icanhazdadjoke.com/search?term=${searchTerm}`,
+        method:'GET'
+
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+           console.log(response.body)
+         
+           const results = JSON.parse(response.body).results
+            fs.writeFile("jokes-2.txt", JSON.stringify(results), ()=>console.log('done'))
+
+        }
+        else {
+            console.log("no joke was found for the search term")
+        }
+    })
+    
+
 })
 
-// let a, b;
-// [a, b, searchTerm] = process.argv;
 
-let randonSearch = (Math.floor(Math.random() * 5))
-// request(`https://icanhazdadjoke.com/search?term=${searchTerm}`, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//         searchResponse = console.log(Math.florr(Math.random() * response.body)
-//         console.log(searchResponse)
-//     }
-//     else {
-//         console.log("no joke was found for the search item")
-//     }
-// })
-
-// fs.writeFile("jokes.txt", function (err) {
-//     console.log(searchResponse)
-// })
 
 
 
